806324362dbb1a7a8d1ccb39ad3b84659ff77deb
deonokongo <lilian.deon29@gmail.com>
2024-04-19 04:56:03 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #user/bin/pup
 #install flask from pip3
 package { 'flask':
-  ensure	=> '2.1.0',
-  provider	=> 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #a manifest that kills a process named killmenow
 exec { 'killmenow_process':
-  command	=> 'pkill -9 killmenow',
-  onlyif	=> 'pgrep killmenow',
+  command => 'pkill -9 killmenow',
+  onlyif  => 'pgrep killmenow',
 }
 
