ec5c4277d471c8d8203b931efee83bb4c4da32d7
deonokongo <lilian.deon29@gmail.com>
2024-04-19 05:46:34 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 #a manifest that kills a process named killmenow
-exec { 'killmenow_process':
-  command => 'pkill -9 killmenow',
-  onlyif  => 'pgrep killmenow',
+exec { 'pkill killmenow':
+  path  => '/usr/bin:/usr/sbin:bin '
 }
-
 
