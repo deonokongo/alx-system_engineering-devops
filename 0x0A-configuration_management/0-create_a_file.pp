fdd6f6cde4fdae4d5ef3375cb2f91a88be41fc86
deonokongo <lilian.deon29@gmail.com>
2024-04-19 04:40:54 -0700

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create /tmp/school file
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
+
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+#user/bin/pup
+#install flask from pip3
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
+}
+
